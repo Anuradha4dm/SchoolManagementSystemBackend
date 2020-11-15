24a8d2152d0cd69512d9ebe17ee358a830dbe2a4
Anuradha4dm <damithanuradha44@gmail.com>
2020-11-15 12:19:11 +0530

    update student results success

--- routes/teacherRoutes.js
+++ routes/teacherRoutes.js
@@ -11,10 +11,19 @@ router.post('/add-student-result', teacherController.postAddStudentResults);
 //POST /teacher/mark-attendence
 router.post('/mark-attendence', teacherController.postMarkStudentAttendence);
 
-//POST
+//POST     //teacher//class-student-list/:id    
 router.get('/class-student-list/:id', teacherController.getStudentListForSpecificTeacher);
 
 router.post('/get-student-subject', teacherController.getSubjectOfStudent);
+
+//GET       /teacher/get-teacher-profile/AC_
+router.get('/get-teacher-profile/:id', teacherController.getGetTeacherDataForProfile);
+
+//POST      /teacher/edit-result   =>this route will get the past result data to update the result by the teacher
+router.post('/edit-results-get-previous', teacherController.postGetPreviousResultData);
+
+//POST /teacher/update-result
+router.post('/update-student-result', teacherController.postUpdateStudentResult);
 
 
 module.exports = router;
