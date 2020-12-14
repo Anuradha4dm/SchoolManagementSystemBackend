2d0b98726053b3b5bb741fc2a0ec998b0dc8bad5
Madushan96 <52341473+Madushan96@users.noreply.github.com>
2020-12-15 01:13:08 +0530

    change database password

--- util/databaseConnection.js
+++ util/databaseConnection.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('schoolmanagementsystembd', 'root', 'damith123', {
+const sequelize = new Sequelize('schoolmanagementsystembd', 'root', '1234', {
     dialect: 'mysql',
     host: 'localhost'
 })
